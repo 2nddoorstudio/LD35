52c670a78fdfa6ecbb078fac177de590e5f1ec4b
sean-lane <sjl5306@psu.edu>
2016-04-16 19:57:26 -0400

    Start of event system, currently borked

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,13 +3,41 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+	public bool b_gamePaused;
+
+	public delegate void GamePause(object sender, System.EventArgs e);
+	public event GamePause Pause;
+
 	// Use this for initialization
 	void Start () {
-	
+
+		b_gamePaused = false;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		// check if we hit escape key
+		if (Input.GetKey(KeyCode.Escape)) {
+			
+			if (b_gamePaused == true) {
+				OnPause ();
+				b_gamePaused = false;
+
+			} else {
+				OnPause ();
+				b_gamePaused = true;
+
+			}
+		}
+	}
+
+	private void OnPause() {
+
+		if (Pause != null) {
+			Pause (this, System.EventArgs.Empty);
+		}
+
 	}
 }
