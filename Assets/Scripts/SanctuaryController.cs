fe542f76346045547c7aac5d7e6aebce7f4c0fa4
gplord <gplord@gmail.com>
2016-04-19 13:44:20 -0400

    Sanctuary circle projector, 3 projector shaders, and adding glow effect into SanctuaryController

--- Assets/Scripts/SanctuaryController.cs
+++ Assets/Scripts/SanctuaryController.cs
@@ -3,9 +3,18 @@ using System.Collections;
 
 public class SanctuaryController : MonoBehaviour {
 
+	[SerializeField]
+	Projector projector;			// Projector component for the glowing circle -- define in editor
+
+	// Defined values for fade speeds -- serialize these fields if desired for editor tweaking
+	float defaultAlpha = 0.35f;
+	float glowFadeInSpeed = 1.5f;
+	float glowFadeOutSpeed = 0.5f;	
+
+
 	// Use this for initialization
 	void Start () {
-	
+		projector.material.color = new Color(0,1,1,defaultAlpha);
 	}
 	
 	// Update is called once per frame
@@ -21,6 +30,9 @@ public class SanctuaryController : MonoBehaviour {
 			return;
 
 		unit.OnSanctuaryTrigger(true, gameObject);
+
+		StartCoroutine(GlowUp());	// Begin glow effect
+
 	}
 
 	void OnTriggerExit(Collider other)
@@ -31,4 +43,38 @@ public class SanctuaryController : MonoBehaviour {
 
 		unit.OnSanctuaryTrigger(false, gameObject);
 	}
+
+	IEnumerator GlowUp() {
+
+		// Cancel any glow coroutines that have already been started
+		StopCoroutine(GlowUp());
+		StopCoroutine(GlowDown());
+
+		// Current alpha value
+		float alpha = projector.material.color.a;
+
+		while (alpha < 1f) {	// 1f is full alpha
+			alpha += glowFadeInSpeed * Time.deltaTime;
+			projector.material.color = new Color(0,1,1,alpha);
+        	yield return new WaitForEndOfFrame();
+		}
+
+		StartCoroutine(GlowDown());
+		yield break;
+	}
+
+	IEnumerator GlowDown() {
+
+		// Current alpha value
+		float alpha = projector.material.color.a;
+
+		while (alpha > defaultAlpha) {
+			alpha -= glowFadeOutSpeed * Time.deltaTime;
+			projector.material.color = new Color(0,1,1,alpha);
+        	yield return new WaitForEndOfFrame();
+		}
+
+		yield break;
+	}
+
 }
