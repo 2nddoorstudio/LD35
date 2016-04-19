dcd88cbc2b62dc73f20637d5715e3bd0b086d6c8
gplord <gplord@gmail.com>
2016-04-19 14:35:49 -0400

    Adds camera rotation using Q and E, commented in CameraController

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -12,6 +12,12 @@ public class CameraController : MonoBehaviour {
 	[SerializeField]
 	float lead = 3.0f;
 
+	[SerializeField]
+	float defaultAngle = 45.0f;
+
+	[SerializeField]
+	float rotationSpeed = 30f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -20,5 +26,13 @@ public class CameraController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.position = Vector3.Lerp(transform.position, target.transform.position + (target.transform.forward * lead), followSpeed);
+
+		// Rotate camera around Up axis, centered on target, with keys Q and E, for left and right, respectively
+		if (Input.GetKey(KeyCode.Q)) {
+			transform.RotateAround(target.transform.position, Vector3.up, rotationSpeed * Time.deltaTime);	
+		} else if (Input.GetKey(KeyCode.E)) {
+			transform.RotateAround(target.transform.position, Vector3.up, -rotationSpeed * Time.deltaTime);	
+		}
+
 	}
 }
