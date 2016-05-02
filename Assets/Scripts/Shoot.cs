c4c823afce7f1581bf5b516b2815c7a6bfab4c2b
gplord <gplord@gmail.com>
2016-05-02 02:09:50 -0400

    New semi-final terrain (for prototype), material, texture, and placeholder trees (randomized until next update)

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -8,6 +8,7 @@ public class Shoot : MonoBehaviour {
 
 	bool shotCharging = false;
 	float shotTimer;
+	float cooldownTimer = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -22,18 +23,36 @@ public class Shoot : MonoBehaviour {
 		}
 
 		if (shotCharging) {
-			if (shotTimer < 2)
+			if (shotTimer < 1.5f)
 				shotTimer += Time.deltaTime;
 			Debug.Log(shotTimer);
 		}
 
+		if (cooldownTimer > 0) {
+			cooldownTimer -= Time.deltaTime;
+		}
+
 		if (Input.GetButtonUp("Fire1")) {
-			Debug.Log("Bang! " + shotTimer);
-			GameObject arrowPrefab = (GameObject) Instantiate(arrow, transform.position + (transform.up) + (transform.forward*2), transform.rotation) as GameObject;
-			arrowPrefab.GetComponent<Rigidbody>().AddForce(transform.forward * (100f * shotTimer), ForceMode.Impulse);
-			arrowPrefab.GetComponent<Rigidbody>().AddForce(transform.up * (10f * shotTimer), ForceMode.Impulse);
-			shotTimer = 0;
-			shotCharging = false;
+			if (cooldownTimer <= 0) {
+				Debug.Log("Bang! " + shotTimer);
+
+				//TODO: Adjust arrow charge time to feel more satisfying
+
+				// Version 1: Arrow has more strength (flies further/straighter) by charging
+				GameObject arrowPrefab = (GameObject) Instantiate(arrow, transform.position + (transform.up) + (transform.forward*2), transform.rotation) as GameObject;
+				arrowPrefab.GetComponent<Rigidbody>().AddForce(transform.forward * (150f * shotTimer), ForceMode.Impulse);
+				arrowPrefab.GetComponent<Rigidbody>().AddForce(transform.up * (15f * shotTimer), ForceMode.Impulse);
+
+				// Version 2: Arrow is more accurate (narrows a target cone to a fixed point) by charging
+	//			Quaternion adjustedAngle = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y+Random.Range(-10f+shotTimer*5f,10f-shotTimer*5), transform.rotation.eulerAngles.z+Random.Range(-10f+shotTimer*5f,10f-shotTimer*5f));
+	//			GameObject arrowPrefab = (GameObject) Instantiate(arrow, transform.position + (transform.up) + (transform.forward*2), adjustedAngle) as GameObject;
+	//			arrowPrefab.GetComponent<Rigidbody>().AddForce(arrowPrefab.transform.forward * (200f), ForceMode.Impulse);
+	//			arrowPrefab.GetComponent<Rigidbody>().AddForce(transform.up * (20f), ForceMode.Impulse);
+
+				shotTimer = 0;
+				shotCharging = false;
+				cooldownTimer = 0.5f;
+			}
 		}
 	
 	}
