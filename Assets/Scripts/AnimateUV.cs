8fb498dab2657f1d8595f5393aea62671d889331
gplord <gplord@gmail.com>
2016-05-09 11:55:37 -0400

    Updated terrain, river, animated water, and waterfalls

---
+++ Assets/Scripts/AnimateUV.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateUV : MonoBehaviour 
+{
+    public int materialIndex = 0;
+    public Vector2 uvAnimationRate = new Vector2( 0f, 0.25f );
+    public string textureName = "_MainTex";
+
+	Vector2 uvOffset = Vector2.zero;
+
+	void Update() {
+		uvOffset += ( uvAnimationRate * Time.deltaTime );
+		if( GetComponent<Renderer>().enabled ) {
+			GetComponent<Renderer>().materials[ materialIndex ].SetTextureOffset( textureName, uvOffset );
+		}
+	}
+
+}
